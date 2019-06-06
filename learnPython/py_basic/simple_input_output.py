4bed932c1fe9c12eb03c5263a741967fa3c361cb
莫非 <mofei@skyguard.com.cn>
2019-06-06 16:37:59 +0800

    Webapp sample

---
+++ learnPython/py_basic/simple_input_output.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+# !/usr/bin/env python3
+
+'''
+# 输出
+# print()函数也可以接受多个字符串，用逗号“,”隔开，就可以连成一串输出
+# print()会依次打印每个字符串，遇到逗号“,”会输出一个空格
+
+# 输入
+# name = input('please enter your name: ')
+
+
+'''
+
+print("1024*768=" + str(1024*768))
+
+name = input()
+print("Welcome to our websites, ", name)
+
