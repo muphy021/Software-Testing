c0cacc6d463d9dec7b3112527fc720111ed3c646
莫非 <mofei@skyguard.com.cn>
2019-07-16 18:24:42 +0800

    optimize path with os.path.dirname(__file__) and os.path.join()

--- learnPython/py_file/py_readfile.py
+++ learnPython/py_file/py_readfile.py
@@ -6,15 +6,19 @@ test python's file operation for reading file.
 import os
 
 # print(os.getcwd())
-file_path = os.getcwd() + "\\os_path.py"
+# file_path = os.getcwd() + "\\os_path.py"
+
+
+# print(os.path.dirname(__file__)) #当前文件所在路径
+current_file_path = os.path.dirname(__file__)
+file_path = os.path.join(current_file_path, "os_path.py")
 print(file_path)
 
-open_file = open(file_path, 'r', True, "utf-8")
-try:
-    while True:
-        content = open_file.readline()
-        if not content:
-            break
-        print(content, end=' ')
-finally:
-    open_file.close()
+open_file = open(file_path, 'r', True)
+
+while True:
+    content = open_file.readline()
+    if not content:
+        break
+    print(content)
+
